dd25e96e12190de03c4af5704b8af8c10783923d
Jie Chen <jiechenmc@gmail.com>
2022-02-22 10:28:36 -0500

    finishedScraping

--- index.js
+++ index.js
@@ -3,22 +3,25 @@ const puppeteer = require("puppeteer");
 const fs = require("fs");
 const path = require("path");
 
-const scrape = async (source, option) => {
+const outDir = "outFiles";
+
+const scrape = async (source, option, subject) => {
+  const outFileName = `${outDir}/${subject}-${option}.tsv`;
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto(source);
 
-  const writeToFile = (data, fileName) => {
+  const writeToFile = (data) => {
     let { rank, uni, totalScore, categoryScore } = data;
     let string = `${rank}\t${uni}\t${totalScore}\t${categoryScore}\n`;
-    if (fs.existsSync(fileName)) {
-      fs.appendFileSync(fileName, string);
+    if (fs.existsSync(outFileName)) {
+      fs.appendFileSync(outFileName, string);
     } else {
-      fs.writeFileSync(fileName, string);
+      fs.writeFileSync(outFileName, string);
     }
   };
 
-  const processTable = async (fileName) => {
+  const processTable = async () => {
     // Init cheerio
     const data = await page.evaluate(
       () => document.querySelector("*").outerHTML
@@ -34,15 +37,12 @@ const scrape = async (source, option) => {
       let totalScore = text[5].trim();
       let categoryScore = text[7].trim();
       // For debugging: console.log(`${ranking}\t${university}\t${totalScore}\t${categoryScore}`);
-      writeToFile(
-        {
-          rank: ranking,
-          uni: university,
-          totalScore: totalScore,
-          categoryScore: categoryScore,
-        },
-        fileName
-      );
+      writeToFile({
+        rank: ranking,
+        uni: university,
+        totalScore: totalScore,
+        categoryScore: categoryScore,
+      });
     });
   };
 
@@ -64,7 +64,7 @@ const scrape = async (source, option) => {
       await page.click(changeScoreSelect);
       await page.click(scoreOption);
     }
-    await processTable(`out/${op}.tsv`);
+    await processTable();
     await page.click(nextPageButton);
   };
 
@@ -79,17 +79,15 @@ const scrape = async (source, option) => {
 
 const cleanDirectory = async () => {
   // directory path
-  const directory = "out";
-
   // check if directory exist
 
-  if (!fs.existsSync(directory)) fs.mkdirSync(directory);
+  if (!fs.existsSync(outDir)) fs.mkdirSync(outDir);
   // delete directory recursively
-  fs.readdir(directory, (err, files) => {
+  fs.readdir(outDir, (err, files) => {
     if (err) console.log(err);
 
     for (const file of files) {
-      fs.unlink(path.join(directory, file), (error) => {
+      fs.unlink(path.join(outDir, file), (error) => {
         if (err) console.log(error);
       });
     }
@@ -98,12 +96,13 @@ const cleanDirectory = async () => {
 
 (async () => {
   let ops = ["Q1", "CNCI", "IC", "TOP", "AWARD"];
-  await cleanDirectory();
+  //await cleanDirectory();
 
   for (const op in ops) {
     scrape(
-      "https://www.shanghairanking.com/rankings/gras/2021/RS0102",
-      ops[op]
+      "https://www.shanghairanking.com/rankings/gras/2021/RS0505",
+      ops[op],
+      "Sociology"
     );
   }
 })();
